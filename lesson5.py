d4f800bff5801e09d56b0cfeac71d1784ab54f3b
Barata-Ribeiro <j.mendes1920@gmail.com>
2023-03-16 10:26:29 -0300

    bool (boolean) data type - True or False

---
+++ lesson5.py
@@ -0,0 +1,11 @@
+# Data type bool (boolean)
+# When querying something in software,
+# there are only two possible answers:
+# yes (True) or no (False).
+# There are several operators for "querying"
+# Among them is ==, a logical operator that
+# asks if one value equals another.
+print(10 == 10) # True => Yes
+print(10 == 12) # False => No
+print(type(10 == 10)) # Returns the type of the value 'bool'
+print(type(True)) # Returns the same as the above
