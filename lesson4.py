0f3e0ca398c5234e85d6c92d747326d8a7149ac5
Barata-Ribeiro <j.mendes1920@gmail.com>
2023-03-16 10:25:47 -0300

    int and float data types (numbers)

---
+++ lesson4.py
@@ -0,0 +1,22 @@
+# Types int and float
+# int -> Integer, a whole number
+# The int type represents any number,
+# positive or negative. int without a sign is
+# positive.
+print(15) # int, integer
+print(-26) # int, integer
+print(0)
+
+# float -> Floating point number
+# The float type represents any positive
+# or negative number with a floating point.
+# float without a sign is positive.
+print(1.56, 1.3) # float, floating point number
+print(0.0, -1.2) # float, floating point number and negative number
+
+# The function (It is called function for 
+# easier comprehension) type represents the 
+# type that Python inferred to the value.
+print(type('Barata')) # <class 'str'> it is a string
+print(type(123)) # <class 'int'> it is a integer number
+print(type(1.5), type(-1.6), type(0.0)) # <class 'float'> it is a floating point number
