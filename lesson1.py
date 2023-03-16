3e7f999e34b7ccc5be1852492d9f9cb16501ad77
Barata-Ribeiro <j.mendes1920@gmail.com>
2023-03-16 10:22:43 -0300

    Commenting & DocString

---
+++ lesson1.py
@@ -0,0 +1,16 @@
+"""
+DocString
+Writing whatever is necessary in here
+Python reads it
+"""
+
+'''
+Also DocString
+Used to explain what is going on
+'''
+
+# Allows the user to comment
+print('Hello, World!') # Prints "Hello, World
+# Above
+print(123) # Prints 123, in line
+# Bellow
