4be87353c88aa04b43d7bb51ba71f04e4759e7ad
Barata-Ribeiro <j.mendes1920@gmail.com>
2023-03-16 10:23:59 -0300

    print function, arguments, linebreak

---
+++ lesson2.py
@@ -0,0 +1,9 @@
+# \r\n return & line feed -> CRLF
+# \n -> LF
+# \r -> CR
+
+print(1001, 1, 1011, sep=' — ', end='##\n')
+print(534, 23, 12, sep=", ", end='\n')
+print(5, 73, 46, sep='', end='\r\n')
+
+# Print() is not defined in Python, "Did you mean to 'print'?"
