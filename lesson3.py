36fbdc206a143eb647c87b6ac5bf0974f137e7a1
Barata-Ribeiro <j.mendes1920@gmail.com>
2023-03-16 10:24:47 -0300

    Typing, strings, quotes

---
+++ lesson3.py
@@ -0,0 +1,24 @@
+"""
+DocString
+Python = Programming language
+Typing = Dynamic / Strong
+str -> string -> text
+Strings are texts within a quotation mark
+"""
+print(1234)
+
+# Single quote / double quotes within single quotes
+print('Barata Ribeiro')
+print(1, 'Barata "Ribeiro"')
+
+# Double quote / single quotes within double quotes
+print("Pindaíba")
+print(2, "Barata 'Ribeiro'")
+
+
+# Both methods are not used
+# Escape (\)
+print("\"escape\" Character")
+
+# r shows the escape character
+print(r"\"escape\" Character")
