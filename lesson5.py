a352c51726da075c915c7b7fe52e69038474a392
Barata-Ribeiro <j.mendes1920@gmail.com>
2023-03-17 09:31:09 -0300

    UPDATED bool (boolean) data type (True or False)

--- lesson5.py
+++ lesson5.py
@@ -5,7 +5,7 @@
 # There are several operators for "querying"
 # Among them is ==, a logical operator that
 # asks if one value equals another.
-print(10 == 10) # True => Yes
-print(10 == 12) # False => No
-print(type(10 == 10)) # Returns the type of the value 'bool'
-print(type(True)) # Returns the same as the above
+print(10 == 10)  # True => Yes
+print(10 == 12)  # False => No
+print(type(10 == 10))  # Returns the type of the value 'bool'
+print(type(True))  # Returns the same as the above
