c8e226ef29c628a17dce7ec385e4b5f8a7383de0
Barata-Ribeiro <j.mendes1920@gmail.com>
2023-03-17 09:22:49 -0300

    Variables and data types Exercise (solved)

---
+++ lesson8.py
@@ -0,0 +1,15 @@
+# EXERCISES FROM THE COURSE
+name = 'João'
+sname = 'M J Barata Ribeiro'
+age = 27
+bday = 1995
+# bday = 2023 - age # not accurate enough, depending on month
+is_legal_age = age >= 18
+height = 1.84
+
+print('Name:', name)
+print('Surname:', sname)
+print('Age:', age)
+print('Year of Birth:', bday)
+print('Is of legal age?', is_legal_age)
+print('Height:', height)
