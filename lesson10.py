345a3c6e0c38d11fe36239a163ab2b47de03b81d
Barata-Ribeiro <j.mendes1920@gmail.com>
2023-03-20 09:19:47 -0300

    Concatenation (+) and repetition (*) with arithmetic operators

---
+++ lesson10.py
@@ -0,0 +1,6 @@
+concatenation = 'A' + 'B' + 'C'  # For numbers use str()
+print(concatenation)  # It prints ABC, only same type
+
+ten_times = 'A' * 10  # Repeats 'A' 10 times
+three_times_name = 'João' * 3  # Repeats 'João' 3 times
+print(ten_times, three_times_name)
