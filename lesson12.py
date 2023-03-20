e044ef839773de4d98ef0c5f5b4e75f55025532d
Barata-Ribeiro <j.mendes1920@gmail.com>
2023-03-20 09:21:59 -0300

    BMI calculation Exercise  (solved)

---
+++ lesson12.py
@@ -0,0 +1,10 @@
+name = 'Barata Ribeiro'
+height = 1.84
+weight = 88
+bmi = weight / (height * height)  # (height ** 2) also works
+
+# Barata Ribeiro is 1.84 meters in height
+print(name, 'is', height, 'meters in height,')
+# weights 88 kilograms and his BMI is...
+# BMI 25.99243856332703
+print('weights', weight, 'kilograms, and his BMI is:', bmi)
