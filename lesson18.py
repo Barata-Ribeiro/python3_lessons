c24a296b802b4800dde0aa07a3ef8126c3e4df4f
Barata-Ribeiro <j.mendes1920@gmail.com>
2023-03-22 08:52:35 -0300

    VS Code Debugger and Python interpreter reading codes

---
+++ lesson18.py
@@ -0,0 +1,37 @@
+# if / elif      / else
+# (portuguese)
+# se / se não se / se não
+
+# THIS IS FOR THE DEBUGGING LESSON!!!
+# IT'S THE SAME FILE AS BEFORE!
+
+
+condition = True
+
+if condition:
+    print('This is the "if" code.')
+else:
+    print('The "else" code is opposite to the "if" code.')
+
+
+condition1 = False
+condition2 = False
+condition3 = True
+condition4 = False
+
+if condition1:
+    print('This is the string of the first condition.')
+elif condition2:
+    print('This is the string of the second condition.')
+elif condition3:
+    print('This is the string of the third condition')
+elif condition4:
+    print('This is the string of the fourth condition')
+else:
+    print('The condition was not met.')
+
+
+if 10 == 10:
+    print('This is another "if" conditional string.')
+
+print('Outside "if" conditional code')
