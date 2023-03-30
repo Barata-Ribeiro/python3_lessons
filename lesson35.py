2070a36d7f6a490acebc266bfb72651ddddbd942
Barata-Ribeiro <j.mendes1920@gmail.com>
2023-03-30 17:06:26 -0300

    while and break - Repetition Structure (Part 2)

---
+++ lesson35.py
@@ -0,0 +1,16 @@
+"""
+repetitions or loops
+while (for repetition)
+Performs an action while a condition is true
+Infinite loop -> When a code has no end
+"""
+counter = 0
+# While counter is less than or equal to
+# the specified number...
+while counter <= 10:
+    # Print the counter value
+    print(counter) # If order is altered, can start from 1
+    # Add 1 to counter
+    counter += 1
+# After loop is over, print the following message
+print("It's over.")
