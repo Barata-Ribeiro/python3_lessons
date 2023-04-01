d47468897b17f34bb1f718e94ef684dbda64a3c3
Barata-Ribeiro <j.mendes1920@gmail.com>
2023-04-01 17:38:48 -0300

    Assignment operators with arithmetic operators

---
+++ lesson36.py
@@ -0,0 +1,20 @@
+"""
+Assignment Operators
+= += -= *= /= //= **= %=
+OBS: I already know them because of JavaScript,
+but will continue reinforcing the knowledge.
+"""
+counter = 0
+counter += 1
+counter -= 2
+counter *= 10
+counter /= 20
+counter **= 25
+counter %= 50
+
+# Only integers
+counter2 = 100
+counter2 //= 2
+
+print(counter)  # Some crazy number!!!
+print(counter2)
