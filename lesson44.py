353f7eed05a0b6e526ec9797c9ea372143f83881
Barata-Ribeiro <j.mendes1920@gmail.com>
2023-04-06 08:44:49 -0300

    'range + for' to use ranges of numbers

---
+++ lesson44.py
@@ -0,0 +1,12 @@
+"""
+For + Range
+range -> range(start, stop, step)
+"""
+
+numbers = range(10)  # ends at 10
+# numbers = range(5, 10)  # starts at 5 and ends at 10
+# numbers = range(2, 10, 2)  # starts at 2 and ends at 10 and step is 2
+
+# In 'for' we don't need index
+for number in numbers:
+    print(number)
